2693e00a1136ab1771097c61a95908dc73a4e9c7
Gabriel <glima7591@gmail.com>
2025-03-27 21:07:06 -0300

    refactor: reorganização das rotas e implementação de controle de acesso
    - Implementação do componente PrivateRoute para controle de acesso baseado em roles
    - Reorganização das rotas em grupos separados para admin e usuário
    - Adição de rotas protegidas para:
      * Admin: dashboard, gerenciamento de tarefas, criação de tarefas e gerenciamento de usuários
      * Usuário: dashboard, tarefas e visualização de detalhes de tarefas
    - Implementação de rotas públicas para login e cadastro

--- frontend/tesqui/src/App.jsx
+++ frontend/tesqui/src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./routes";
+import {
+  Login,
+  SignUp,
+  UserDashboard,
+  MyTasks,
+  Dashboard,
+  ViewTaskDetails,
+  ManageTasks,
+  CreateTask,
+  ManageUsers,
+} from "./pages/index.js";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
 
-export default App
+        {/* Rota apenas para admin */}
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route path='/admin/dashboard' element={<Dashboard />} />
+          <Route path='/admin/tasks' element={<ManageTasks />} />
+          <Route path='/admin/create-task' element={<CreateTask />} />
+          <Route path='/admin/users' element={<ManageUsers />} />
+        </Route>
+
+        {/* Rota apenas para usuários */}
+        <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+          <Route path='/user/dashboard' element={<UserDashboard />} />
+          <Route path='/user/tasks' element={<MyTasks />} />
+          <Route path='/user/tasks-details/:id' element={<ViewTaskDetails />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/tesqui/src/pages/index.js
@@ -0,0 +1,26 @@
+// Rotas de autenticação
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
+
+// Rotas de usuário
+import UserDashboard from "./User/UserDashboard";
+import MyTasks from "./User/MyTasks";
+import ViewTaskDetails from "./User/ViewTaskDetails";
+
+// Rotas de admin
+import Dashboard from "./Admin/Dashboard";
+import ManageTasks from "./Admin/ManageTasks";
+import CreateTask from "./Admin/CreateTask";
+import ManageUsers from "./Admin/ManageUsers";
+
+export {
+  Login,
+  SignUp,
+  UserDashboard,
+  MyTasks,
+  ViewTaskDetails,
+  Dashboard,
+  ManageTasks,
+  CreateTask,
+  ManageUsers,
+};
